a3b66a4b2141d0a9519fab0d0aa693deac4d0826
CarlJSL <carlossipanlozano@gmail.com>
2025-07-06 18:00:04 -0500

    cambios en la configuración de la base de datos

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'host' => 'mysql-db',    // 'host' => '127.0.0.1',
+    'host' => '127.0.0.1',    // 'host' => '127.0.0.1',
     'dbname' => 'tecnovedades',
-    'username' => 'user',   // 'username' => 'root
-    'password' => 'password', // 'password' => '',
+    'username' => 'root',   // 'username' => 'root
+    'password' => '', // 'password' => '',
 ];
