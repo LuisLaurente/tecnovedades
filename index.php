cdc9a0464edb28f868888c9c954d538f9182dfb6
Luis Laurente <137670663+LuisLaurente@users.noreply.github.com>
2025-07-24 13:50:12 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-echo "TEST MINIMO - PHP FUNCIONANDO";
-?>
+// Autoload de clases
+require_once __DIR__ . '/autoload.php';
+
+// Cargar el router
+require_once __DIR__ . '/core/Router.php';
+
+use Core\Router;
+
+// Obtener la ruta desde la URL
+$url = $_GET['url'] ?? 'producto/index'; // ruta por defecto
+
+$router = new Router();
+$router->handleRequest($url);
