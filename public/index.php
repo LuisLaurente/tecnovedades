8f55e83492d6aded9d772b0ec0fa1a64d9c1b62e
Luis Laurente <137670663+LuisLaurente@users.noreply.github.com>
2025-07-24 14:36:46 -0500

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,2 +1,29 @@
 <?php
-echo "¡Hola desde Railway!";
+session_start(); 
+// Autocarga de clases
+require_once __DIR__ . '/../core/autoload.php';
+
+
+// Activar errores en modo desarrollo
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+
+// Obtener solo la ruta relativa limpia desde la UR
+$url = isset($_GET['url']) ? $_GET['url'] : 'home/index';
+
+// Si está vacía, usar ruta por defecto 2
+if ($url === '') {
+    $url = 'home/index';
+}
+
+// Inyectar en $_GET['url'] para compatibilidad con el router
+$_GET['url'] = $url;
+
+// Iniciar el router
+use Core\Router;
+
+$router = new Router();
+$router->handleRequest($url);
